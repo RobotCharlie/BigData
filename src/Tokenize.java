0f1587186b0b372d3ef7a5857bbc1ccac4216b48
Charlie Gao <charles.gao.92@gmail.com>
2015-07-07 23:56:38 -0400

    first commit

---
+++ src/Tokenize.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+
+public class Tokenize {
+
+	public static void main(String args[]) {
+
+		// Read input file
+		File file = new File(
+				"/Users/CharlesGao/Projects/cc-example/tweet_input/tweets.txt");
+		// Use a TreeSet<String> which will automatically sort the words
+		// in alphabetical order
+		Set<String> wordsInTweet = new TreeSet<>();
+
+		try {
+			Scanner tokenizeScanner = new Scanner(new FileReader(file));
+			tokenizeScanner.useDelimiter(" ");
+			while (tokenizeScanner.hasNext()) {
+				String singleWordInTweet = tokenizeScanner.next();
+				if (!singleWordInTweet.equals("")) {
+					// process only non-empty strings
+					// convert to lowercase and then add to the set
+					wordsInTweet.add(singleWordInTweet.toLowerCase());
+				}
+			}
+			// Now wordsInTweet are in alphabetical order without duplicates,
+			// print the wordsInTweet separating them in lines
+			for (String eachWord : wordsInTweet) {
+				System.out.print(eachWord + "\r\n");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
