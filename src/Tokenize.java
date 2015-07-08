9137f32eb0762fd22ef27c7863079b82a07fdf2f
Charlie Gao <charles.gao.92@gmail.com>
2015-07-08 11:30:05 -0400

    start task2

--- src/Tokenize.java
+++ src/Tokenize.java
@@ -12,28 +12,45 @@ public class Tokenize {
 				"/Users/CharlesGao/Projects/cc-example/tweet_input/tweets.txt");
 		// Use a TreeSet<String> which will automatically sort the words
 		// in alphabetical order
-		Set<String> wordsInTweet = new TreeSet<>();
-
+//		Set<String> wordsInTweet = new TreeSet<>();
+//		try {
+//			Scanner tokenizeScanner = new Scanner(new FileReader(file));
+//			tokenizeScanner.useDelimiter(" ");
+//			while (tokenizeScanner.hasNext()) {
+//				String singleWordInTweet = tokenizeScanner.next();
+//				if (!singleWordInTweet.equals("")) {
+//					// process only non-empty strings
+//					// convert to lowercase and then add to the set
+//					wordsInTweet.add(singleWordInTweet.toLowerCase());
+//				}
+//			}
+//			// Now wordsInTweet are in alphabetical order without duplicates,
+//			// print the wordsInTweet separating them in lines
+//			for (String eachWord : wordsInTweet) {
+//				System.out.print(eachWord + "\r\n");
+//			}
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		List<String> Tweets = new ArrayList<>();
 		try {
-			Scanner tokenizeScanner = new Scanner(new FileReader(file));
-			tokenizeScanner.useDelimiter(" ");
-			while (tokenizeScanner.hasNext()) {
-				String singleWordInTweet = tokenizeScanner.next();
-				if (!singleWordInTweet.equals("")) {
-					// process only non-empty strings
-					// convert to lowercase and then add to the set
-					wordsInTweet.add(singleWordInTweet.toLowerCase());
-				}
+			Scanner singleTweetScanner = new Scanner(new FileReader(file));
+			singleTweetScanner.useDelimiter("\\n");
+			while (singleTweetScanner.hasNext()) {
+				String singleTweet = singleTweetScanner.next();
+				Tweets.add(singleTweet);
+				
 			}
-			// Now wordsInTweet are in alphabetical order without duplicates,
-			// print the wordsInTweet separating them in lines
-			for (String eachWord : wordsInTweet) {
-				System.out.print(eachWord + "\r\n");
+			for (String eachTweet : Tweets) {
+				System.out.print(eachTweet + "\r\n");
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 
 	}
 
